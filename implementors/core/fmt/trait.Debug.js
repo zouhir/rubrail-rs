777ef96696c1bfd614e052159c4a202c89068337
Trevor Bentley <trevor@trev-minimac.local>
2018-04-02 13:46:31 +0200

    Release {{version}}

--- implementors/core/fmt/trait.Debug.js
+++ implementors/core/fmt/trait.Debug.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["rubrail"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"enum\" href=\"rubrail/enum.SwipeState.html\" title=\"enum rubrail::SwipeState\">SwipeState</a>",];
+implementors["rubrail"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"enum\" href=\"rubrail/enum.SwipeState.html\" title=\"enum rubrail::SwipeState\">SwipeState</a>",synthetic:false,types:["rubrail::interface::SwipeState"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
